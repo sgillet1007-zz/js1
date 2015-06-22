6fca8e5ad2f5c263febc874636a79b86b50ef65f
sgillet1007 <samuelgillet@hotmail.com>
2015-06-22 12:16:30 -0600

    finished loop practice #1

--- js1.js
+++ js1.js
@@ -1,1 +1,44 @@
-alert('Hello Boulder!');
+// alert('Hello Boulder!');
+// var person = prompt("Please enter your name","defaultText");
+
+// alert('Hello ' + person);
+
+
+
+// ******** LOOP PRACTICE #1 *******
+// var i;
+// for (i=0; i<=500;i+=100){
+// 	console.log(i);
+// }
+
+// var i;
+// for (i=0; i<7; i++) {
+// 		console.log(Math.pow(2,i));
+// }
+
+// var i;
+// for (i=1; i<4; i++) {
+// 	console.log(i,i,i);
+// }
+
+// var i;
+// for (i=0;i<=10;i+=2) {
+// 	console.log(i);
+// }
+
+// var i;
+// for (i=3;i<=15;i+=3) {
+// 	console.log(i);
+// }
+
+// var i;
+// for (i=9;i>=0;i--) {
+// 	console.log(i);
+// }
+
+var i;
+for (i=0;i<3;i++) {
+	for (j=0;j<4;j++) {
+		console.log(j);
+	}
+}
